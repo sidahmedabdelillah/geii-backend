ebecd9cfe66056f1c1d43d5542677ea6f39fcdd0
sidahmedabdelillah <s_bentegar@enst.dz>
2020-08-09 04:20:58 +0100

    from wsl

--- api/articles/models/articles.js
+++ api/articles/models/articles.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const axios = require("axios");
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      console.log("here");
+      const url = `www.geii.me/news/${result.id}`;
+      const message = `📍 Checkout our LATEST article titled ${result.Title} 💻🖥📍`;
+      await axios.post(
+        " https://maker.ifttt.com/trigger/newPost/with/key/cnGSkCVRh7gApBwwJo5-PE ",
+        { value1: url, value2: message }
+      );
+      await axios.post(
+        " https://maker.ifttt.com/trigger/mail/with/key/cnGSkCVRh7gApBwwJo5-PE ",
+        { value2: url, value1: message }
+      );
+    },
+  },
+};
